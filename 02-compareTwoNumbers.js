296d14b1646a540da3d9293a53c2fc7249706f25
al cotton <alvicoto@gmail.com>
2025-03-07 14:35:07 -0600

    campare two numbers

--- 02-compareTwoNumbers.js
+++ 02-compareTwoNumbers.js
@@ -21,9 +21,20 @@
 //     should evaluate to "The numbers are the same!"
 
 
-function compareTwoNumbers() {
-
+function compareTwoNumbers(num1, num2) {
+  if(num1 > num2) {
+    return "The first number was bigger!"
 }
+  if(num1 < num2) {
+    return "The second number was bigger!"
+    }
+      if(num1 === num2) {
+    return "The numbers are the same!"
+}
+
+ };
+
+
 
 try {
   module.exports = compareTwoNumbers;
